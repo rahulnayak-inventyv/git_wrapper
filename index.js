977d88c09053d951b99f56521b2309865345e1ba
rahul nayak <rahul.nayak@inventyv-git.com>
2024-08-28 18:24:17 +0530

    committed by code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let gitWrapper = require('./gc-git-wrapper.js')
 
-gitWrapper.status("/home/rahul/Desktop/infra-tibdhistoryimporter").then((res) => {
+gitWrapper.status("/home/rahul/Desktop/Rust_code/git_wrapper_rust").then((res) => {
     console.log(res);
 
 }).catch((error) => {
